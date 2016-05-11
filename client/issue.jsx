4ef63f0abfb9c185e0ec4f583d65735a363cb082
Ade Bateman <bateman@acm.org>
2016-05-11 05:49:46 -0700

    More refactoring

--- client/issue.jsx
+++ client/issue.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
-import Label from './label.jsx';
 import IssueTitle from './issuetitle.jsx';
-import CategoryPicker from './categorypicker.jsx';
 import Notes from './notes.jsx';
-import Button from 'react-bootstrap/lib/Button';
-import Glyphicon from 'react-bootstrap/lib/Glyphicon';
 import Collapse from 'react-bootstrap/lib/Collapse';
-import safeMarkdown from './safe-markdown.jsx';
 import Panel from 'react-bootstrap/lib/Panel';
+import safeMarkdown from './safe-markdown.jsx';
+
+const LabelBadge = ({label}) => {
+	return (
+		<a href={label.url}><span className="badge" style={ {backgroundColor: '#'+label.color, marginRight: "10px", color:"black"} }>
+		  {label.name}
+		</span></a>
+	);
+};
 
 const LabelList = (labels) =>
 	labels.map(function(label) {
-		return <Label key={label.name} label={label} />;
+		return <LabelBadge key={label.name} label={label} />;
 	});
 
 const IssueDetails = (props) =>
@@ -23,18 +27,13 @@ const IssueDetails = (props) =>
 		</div></div>
 	</Collapse>;
 
-const ExpandCollapseButton = (props) =>
-    <Button bsSize="small" onClick={props.onClick} aria-label="Expand">
-      <Glyphicon glyph={props.open ? "minus" : "plus"} />
-    </Button>;
-
-
 export default class Issue extends React.Component {
 	constructor(...args) {
 		super(...args);
 		this.state = { open:false };
 		this.changeCategory = this.changeCategory.bind(this);
 		this.updateNotes = this.updateNotes.bind(this);
+		this.toggleOpen = this.toggleOpen.bind(this);
 	}
 
 	shouldComponentUpdate(nextProps, nextState) {
@@ -55,20 +54,19 @@ export default class Issue extends React.Component {
 
 	render() {
 		var issue = this.props.issue;
-		var title = <h3 style={{display:"flex",justifyContent:"space-between",alignItems:"center"}}>
-			<IssueTitle issue={issue} />
-			<span style={{minWidth:"120px",textAlign:"right"}}>
-				<ExpandCollapseButton open={this.state.open} onClick={ ()=> this.setState({ open: !this.state.open })} />
-		        &nbsp;
-		        <CategoryPicker id={issue.number} category={issue.tracker.category} onPick={this.changeCategory} />
-	        </span>
-		</h3>;
+		var title = <IssueTitle issue={issue} open={this.state.open}
+			onExpandCollapse={this.toggleOpen}
+			onChangeCategory={this.changeCategory} />;
 		return (
 			<Panel header={title} bsStyle="info" style={{marginBottom:"10px"}}>
 				<IssueDetails open={this.state.open} issue={issue} />
 		        <Notes notes={issue.tracker.notes} onUpdate={this.updateNotes} />
 			</Panel>
 		)
+	}
+
+	toggleOpen() {
+		this.setState({ open: !this.state.open });
 	}
 
 	changeCategory(category) {
--- client/label.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default ({label}) => {
-	return (
-		<a href={label.url}><span className="badge" style={ {backgroundColor: '#'+label.color, marginRight: "10px", color:"black"} }>
-		  {label.name}
-		</span></a>
-	);
-};
